3d0db740d40a49c33d7e39f66a09ed2dfa82ba10
Dmitry Tarasev <dmitrytarasev@gmail.com>
2014-12-18 15:05:02 -0500

    PNG Image loading added
    Image can be loaded by calling loadImage(filename) using repl.
    Currently assumes path is same as module.
    
    Also added a test pattern.

--- Code/app.js
+++ Code/app.js
@@ -1,15 +1,25 @@
-var five = require("johnny-five"),
-    board, mirrorMotor;
 
-board = new five.Board();
+var five  = require("johnny-five"),
+    fs    = require('fs'),
+    png   = require('png-js'),
+    board, 
+    mirrorMotor,
+    image = null;
 
 //The number of physical lasers we have
 //Also the number of lines we project
 var NUM_LASERS = 16;
 
+
+board = new five.Board();
+
 board.on("ready", function() {
 
 	console.log("Board ready!");
+
+  this.repl.inject({
+    loadImage : loadImage
+  });
 
   //The motor that the mirror is attached to
   mirrorMotor = new five.Motor({
@@ -25,8 +35,19 @@ board.on("ready", function() {
     console.log( "stopped", timestamp );
   });
 
- 
-
- 
-
 });
+
+//Load the image
+var loadImage = function(filename){
+
+  var path = __dirname + "/" + filename;
+
+  console.log("Loading : " + path);
+
+  try {
+    image = png.load(path);
+  }
+  catch (e) {
+    console.log("Error in loadImage : " + e);
+  }
+};
